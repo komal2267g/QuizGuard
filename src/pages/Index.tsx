4aeef1cd7f17e1bc3fd413baaa15660051e0e2b4
komal2267g <komalchaurasiya637@gmail.com>
2025-08-14 21:55:22 +0530

    Initial commit of QuizGuard project with all files

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -145,7 +145,7 @@ const Index = () => {
             <div className="container mx-auto px-4">
               <div className="grid md:grid-cols-2 gap-8 items-center">
                 <div className="rounded-2xl overflow-hidden">
-                  <img src="https://images.unsplash.com/photo-1516534775068-ba3e7458af70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=80" 
+                  <img src="student.jpg" 
                        alt="Student taking quiz on tablet" 
                        className="w-full h-[400px] object-cover rounded-2xl" />
                 </div>
